f5455a47c1945e186aecc9597094774083fa3020
april <martinezapril56@yahoo.com>
2018-06-09 19:43:19 -0400

    commit

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const cors = require( 'cors' );
+const actions = require( './data/helpers/actionModel' );
+const project = require( './data/helpers/projectModel' );
+const port = 5000;
+const server = express();
+server.use( express.json() );
+server.use( cors() );
+server.get( '/', ( req, res ) =>
+{
+    res.send( 'hello from server' );
 
+})
+server.listen( port, () =>
+{
+    (`server running...${port}`)
+})
